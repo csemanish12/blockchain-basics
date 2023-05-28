312168916b16b9b6be29b0a37fc417197096c79a
Manish sah <sahmanish20@gmail.com>
2023-05-28 10:09:28 +0545

    store funders and amount funded

--- solidity/FundMe.sol
+++ solidity/FundMe.sol
@@ -7,14 +7,18 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
-    uint256 public minimumUsd = 50 * 1e18;
+    uint256 public minimumUsd = 5 * 1e18;
 
     function fund() public payable {
         // want to be able to set a minimum fund amount in USD
+        address[] public funders;
+        mapping(address => uint256) public addressToAmountFunded;
         
         require (msg.value >= minimumUsd, "Didn't send enough"); // 1e18 == 1 * 10 ** 18
         // require keyword is a checker which reverts if false
         // reverting: undo any action before, and send remaining gas back 
+        funders.push(msg.sender)
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
     function getPrice() public view returns (uint256){
