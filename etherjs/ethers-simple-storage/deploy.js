8023197ce275902122a6458b7b9f45b571a2a034
Manish sah <sahmanish20@gmail.com>
2023-05-31 20:40:45 +0545

    create compile script for compiling contracts

--- etherjs/ethers-simple-storage/deploy.js
+++ etherjs/ethers-simple-storage/deploy.js
@@ -1,5 +1,12 @@
-function main() {
+// deploy a contract ? wait for it to be deployed
+async function main() {
   console.log("hi");
 }
 
-main();
+main()
+  .then(() => ProcessingInstruction.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+ 
