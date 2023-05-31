5e83b51b2007c09ca200a092252b34ddb44a1acb
Manish sah <sahmanish20@gmail.com>
2023-05-31 07:51:56 +0545

    add an example of receive and fallback

--- solidity/FundMe.sol
+++ solidity/FundMe.sol
@@ -60,7 +60,7 @@ contract FundMe {
 
     modifier onlyOwner {
         // require(msg.sender == i_owner, "Sender is not owner!");
-        if(msg.sender != i_owner) {revert NotOwner();}
+        if(msg.sender != i_owner) {revert NotOwner();} // more gas efficient than above
         _;
     }
 
