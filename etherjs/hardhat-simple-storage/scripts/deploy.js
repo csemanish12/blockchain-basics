56ea82c02ec3266bc68327e8301cde784a17a318
Manish sah <sahmanish20@gmail.com>
2023-06-04 08:16:18 +0545

    interacting with contract

--- etherjs/hardhat-simple-storage/scripts/deploy.js
+++ etherjs/hardhat-simple-storage/scripts/deploy.js
@@ -12,9 +12,19 @@ async function main() {
     console.log(`deployed contract to:${simpleStorage.address}`)
     
     if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY){
+      console.log("waiting for block confirmation")
       await simpleStorage.deployTransaction.wait(6)
       await verify(simpleStorage.address, [])
     }
+
+    const currentValue = await simpleStorage.retrieve()
+    console.log(`current value is ${currentValue}`)
+
+    const transactionResponse = await simpleStorage.store(7)
+    await transactionResponse.wait(1)
+
+    const updatedValue = await simpleStorage.retrieve()
+    console.log(`updated value is ${updatedValue}`)
 }
 
 async function verify(contractAddress, args) {
