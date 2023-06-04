af509ec5bb9a82f21a4894afa53de6a99aed690c
Manish sah <sahmanish20@gmail.com>
2023-06-04 08:10:01 +0545

    automatically verify contract after deployment

--- etherjs/hardhat-simple-storage/scripts/deploy.js
+++ etherjs/hardhat-simple-storage/scripts/deploy.js
@@ -1,15 +1,36 @@
 // imports
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 // async main
 async function main() {
-    const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    )
     console.log("Deploying contracts")
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
-
     console.log(`deployed contract to:${simpleStorage.address}`)
+    
+    if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY){
+      await simpleStorage.deployTransaction.wait(6)
+      await verify(simpleStorage.address, [])
+    }
+}
 
+async function verify(contractAddress, args) {
+    console.log("verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("already verified")
+        } else {
+            console.log(e)
+        }
+    }
 }
 
 // main
